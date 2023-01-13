90bbb072e0b60aa5165353dbdc45c56d4f9cccf9
Anna Aard <anna.mar00722@gmail.com>
2023-01-13 21:47:46 +0600

    005 family

---
+++ 006 fam/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine ("Ведите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "аня")
+{
+Console.WriteLine ("Ура, Хозяйка вернулась!");
+}
+
+else if (username.ToLower() == "аард")
+{
+Console.WriteLine ("Ты как зашла? У тебя же лапки!");
+}
+
+else if (username.ToLower() == "нуржан")
+{
+Console.WriteLine ("Ты замечательный, но у тебя есть свой комп");
+}
+
+else if (username.ToLower() == "федя")
+{
+Console.WriteLine ("А мама знает чем ты тут занимаешься?");
+}
+
+else
+{
+Console.Write (username);
+Console.Write (", ты кто такой?! Давай досвидания!");
+}
