45b40f7ab045f5865db604d26b9bc4a7f7160927
Anna Aard <anna.mar00722@gmail.com>
2023-01-13 19:29:09 +0600

    003 sum

---
+++ 003 sum/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число ");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA + numberB;
+Console.Write($"{numberA}+{numberB}={result}");
