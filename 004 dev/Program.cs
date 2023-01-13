774dea51258222182ab83dfefea197cf38a27cf3
Anna Aard <anna.mar00722@gmail.com>
2023-01-13 19:40:54 +0600

    004 dev

---
+++ 004 dev/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число ");
+
+double numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+
+double numberB = Convert.ToInt32(Console.ReadLine());
+double result = numberA / numberB;
+Console.Write($"{numberA}/{numberB}={result}");
