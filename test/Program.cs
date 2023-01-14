1f1fc770aa9793b8f1ce83ab14fcdcb8c4355492
Anna Aard <anna.mar00722@gmail.com>
2023-01-14 12:35:28 +0600

    008

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int xa = 20, ya = 1,
+    xb = 1, yb = 20,
+    xc = 40, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
